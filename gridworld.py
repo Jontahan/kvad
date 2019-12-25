04281e6d14e055cdaddd2ad067f16ff7aaecb1d6
Jontahan <jona.jorg@gmail.com>
2019-12-25 20:38:37 +0100

    Fix corner and dead end pushing of boxes

--- gridworld.py
+++ gridworld.py
@@ -9,7 +9,9 @@ colors = {
     'wall' : (50, 50, 50),
     'wall_edge' : (30, 30, 30),
     'box' : (80, 60, 0),
-    'box_edge' : (60, 40, 0)
+    'box_edge' : (60, 40, 0),
+    'goal' : (10, 10, 10),
+    'goal_edge' : (20, 20, 20)
 }
 
 class Gridworld(gym.Env):
@@ -23,54 +25,73 @@ class Gridworld(gym.Env):
         
         self.board = np.array(board)
         self.board_interactive = np.array(board)
-        self.board_interactive[6][5] = 1
-        for i in range(1, width - 1):
-            self.board[4][i] = 1
+        self.board_goal = np.array(board)
         
+        # Wall
+        for i in range(1, width - 1):
+            self.board[2][i] = 1
+
         self.size = len(board)
         self.cell_size = scale
         self.screen = None
-        self.agent_pos = (0, 0)
-    
+
+        self.agent_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        while self.board[self.agent_pos[1]][self.agent_pos[0]] == 1:
+            self.agent_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        
+        box_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        while self.board[box_pos[1]][box_pos[0]] == 1 or box_pos == self.agent_pos:
+            box_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        
+        self.board_interactive[box_pos[1]][box_pos[0]] = 1
+        
+        goal_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        while self.board[goal_pos[1]][goal_pos[0]] == 1 or goal_pos == self.agent_pos or goal_pos == box_pos:
+            goal_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        
+        self.board_goal[goal_pos[1]][goal_pos[0]] = 1
+        
     def reset(self):
         board = np.zeros((self.height, self.width))
         self.board = np.array(board)
-        self.falling_piece_pos = (np.random.randint(0, self.width - 3), 0)
-        self.falling_piece_shape = self.piece_types[np.random.randint(0, len(self.piece_types))]
-        self.subframe = 0
-
-        piece = np.array(np.zeros((self.height, self.width)))
-        for i in range(4):
-            for j in range(4):
-                if self.falling_piece_shape[j][i] == 1:
-                    pos = (i + self.falling_piece_pos[0], j + self.falling_piece_pos[1])
-                    piece[pos[1]][pos[0]] = 1
-        self.time = 0
-
+        self.board_interactive = np.array(board)
+        self.board_goal = np.array(board)
         
-        timing_layer = np.zeros((self.height, self.width))
+        for i in range(1, self.width - 1):
+            self.board[2][i] = 1
         
+        self.agent_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        while self.board[self.agent_pos[1]][self.agent_pos[0]] == 1:
+            self.agent_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        
+        box_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        while self.board[box_pos[1]][box_pos[0]] == 1 or box_pos == self.agent_pos:
+            box_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        
+        self.board_interactive[box_pos[1]][box_pos[0]] = 1
+        
+        goal_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        while self.board[goal_pos[1]][goal_pos[0]] == 1 or goal_pos == self.agent_pos or goal_pos == box_pos:
+            goal_pos = (np.random.randint(0, self.width), np.random.randint(0, self.height))
+        
+        self.board_goal[goal_pos[1]][goal_pos[0]] = 1
+        
+
+        agent_pos_matrix = np.zeros((self.height, self.width))
+        agent_pos_matrix[self.agent_pos[1]][self.agent_pos[0]] = 1
+
         state = np.array([[
             self.board,
-            piece,
-            timing_layer
+            self.board_interactive,
+            self.board_goal,
+            agent_pos_matrix
         ]])
 
         return state
         
-    def resolve_lines(self):
-        removed = 0
-        for i in range(len(self.board)):
-            line = self.board[i]
-            if all(x == 1 for x in line):
-                removed = removed + 1
-                for j in range(i - 1):
-                    self.board[i - j] = self.board[i - j - 1]
-        return removed
-        
     def step(self, action):
         done = False
-        reward = 0
+        reward = -1
         
         target_x, target_y = self.agent_pos
 
@@ -95,19 +116,66 @@ class Gridworld(gym.Env):
                         self.agent_pos = (target_x, target_y)
                         self.board_interactive[target_y][target_x] = 0
                         self.board_interactive[box_target_y][box_target_x] = 1
+                else:
+                    move, target_x, target_y, box_target_x, box_target_y = self.resolve_corner(target_x, target_y, box_target_x, box_target_y)
+                    if move:
+                        self.agent_pos = (target_x, target_y)
+                        self.board_interactive[target_y][target_x] = 0
+                        self.board_interactive[box_target_y][box_target_x] = 1
+                
+                if box_target_x in range(self.width) and box_target_y in range(self.height) and self.board_goal[box_target_y][box_target_x] == 1:
+                    done = True
+                    reward = 10
+
+                        
             elif self.board[target_y][target_x] == 0:
                 self.agent_pos = (target_x, target_y)
 
         agent_pos_matrix = np.zeros((self.height, self.width))
+        agent_pos_matrix[self.agent_pos[1]][self.agent_pos[0]] = 1
 
-        state = [
+        state = np.array([[
             self.board,
             self.board_interactive,
+            self.board_goal,
             agent_pos_matrix
-        ]
+        ]])
 
         return state, reward, done, {}
     
+    def resolve_corner(self, target_x, target_y, box_target_x, box_target_y):
+        diff_x = target_x - box_target_x 
+        diff_y = target_y - box_target_y
+        
+        if diff_x == 0: # vertical
+            left = target_x - 1 not in range(self.width) or \
+                self.board[target_y][target_x - 1] == 1 or \
+                self.board_interactive[target_y][target_x - 1] == 1
+            right = target_x + 1 not in range(self.width) or \
+                self.board[target_y][target_x + 1] == 1 or \
+                self.board_interactive[target_y][target_x + 1] == 1
+            if left:
+                return True, target_x, target_y, target_x + 1, target_y
+            if right:
+                return True, target_x, target_y, target_x - 1, target_y
+            return True, target_x, target_y, target_x, target_y + diff_y
+                
+        if diff_y == 0: # horizontal
+            up = target_y - 1 not in range(self.height) or \
+                self.board[target_y - 1][target_x] == 1 or \
+                self.board_interactive[target_y - 1][target_x] == 1
+            down = target_y + 1 not in range(self.height) or \
+                self.board[target_y + 1][target_x] == 1 or \
+                self.board_interactive[target_y + 1][target_x] == 1
+            if up:
+                return True, target_x, target_y, target_x, target_y + 1
+            if down:
+                return True, target_x, target_y, target_x, target_y - 1
+            return True, target_x, target_y, target_x + diff_x, target_y
+            
+        return False, target_x, target_y, box_target_x, box_target_y
+                
+
     def draw(self, screen, heatmap=None):
         for i in range(len(self.board[0])):
             for j in range(len(self.board)):
@@ -119,6 +187,9 @@ class Gridworld(gym.Env):
                 elif self.board_interactive[j][i] == 1: 
                     pg.draw.rect(screen, colors['box'], cell)
                     pg.draw.rect(screen, colors['box_edge'], cell, 1)
+                elif self.board_goal[j][i] == 1: 
+                    pg.draw.rect(screen, colors['goal'], cell)
+                    pg.draw.rect(screen, colors['goal_edge'], cell, 1)
                 else:
                     pg.draw.rect(screen, colors['floor'], cell)
                     pg.draw.rect(screen, colors['floor_edge'], cell, 1)
--- train_dqn.py
+++ train_dqn.py
@@ -1,13 +1,67 @@
 from gridworld import Gridworld
 import pygame as pg
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from vicero.algorithms.deepqlearning import DQN
 
 scale = 32
-env = Gridworld(scale, width=8, height=8)
+env = Gridworld(scale, width=5, height=5)
 
 pg.init()
 screen = pg.display.set_mode((scale * len(env.board[0]), scale * len(env.board)))
 env.screen = screen
+clock = pg.time.Clock()
 
+"""
 while True:
     env.step(env.action_space.sample())
     env.render()
+"""
+
+def plot(history):
+        plt.figure(2)
+        plt.clf()
+        durations_t = torch.DoubleTensor(history)
+        plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Duration')
+        plt.plot(durations_t.numpy(), c='lightgray', linewidth=1)
+
+        his = 100
+        if len(durations_t) >= his:
+            means = durations_t.unfold(0, his, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(his - 1), means))
+            plt.plot(means.numpy(), c='green')
+            
+        plt.pause(0.001)
+
+class PolicyNet(nn.Module):
+    def __init__(self):
+        super(PolicyNet, self).__init__()
+        
+        self.conv = nn.Conv2d(4, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 8, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(72, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 32)
+        self.fc4 = nn.Linear(32, 5)
+        
+    def forward(self, x):
+        x = F.relu(self.conv(x))
+        #x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = torch.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+dqn = DQN(env, qnet=PolicyNet().double(), plotter=plot, render=True, memory_length=20000, gamma=.92, alpha=.0001, epsilon_start=0.5)
+dqn.train(1000, 4, plot=True, verbose=True)
