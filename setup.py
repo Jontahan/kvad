b5326be23de7ca0841ef0d7409f73bcf24185889
Jonathan Jørgensen <jona.jorg@gmail.com>
2020-05-17 09:14:41 +0200

    Enable installation by pip

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='kvad', 
+      version='0.0.1', 
+      author='Jonathan Jørgensen',
+      packages=find_packages(),
+      py_modules=['kvad'])
